c7f2f2671c3da1824aba45355623a5eaae6b364a
Yen-Ling Kuo <ylkuo@csail.mit.edu>
2018-02-06 13:15:45 -0500

    extract time series from the coinvestment network

--- create_network.py
+++ create_network.py
@@ -3,8 +3,17 @@ import sqlite3
 
 from collections import defaultdict
 from copy import deepcopy
+from datetime import datetime
+from dateutil import relativedelta
 from operator import itemgetter
-from time import strftime, strptime
+from time import mktime, strftime, strptime
+
+def month_year_iter(start_month, start_year, end_month, end_year):
+    ym_start = 12*start_year + start_month - 1
+    ym_end = 12*end_year + end_month - 1
+    for ym in range(ym_start, ym_end):
+        y, m = divmod(ym, 12)
+        yield y, m+1
 
 class CrunchbaseData(object):
     def __init__(self, db_path):
@@ -83,15 +92,50 @@ class CrunchbaseData(object):
                         out_network[i1][i2]['time'][node] = strftime('%Y-%m-%d', ts)
         return out_network
 
+    def generate_time_series(self, in_network, filter_by_nodes=None):
+        if filter_by_nodes is not None:
+            network = in_network.subgraph(filter_by_nodes)
+        else:
+            network = in_network
+        # get the max and min timestamp in this network
+        min_time, max_time = None, None
+        for i1, i2 in network.edges():
+            for c, c_t in network[i1][i2]['time'].items():
+                t = strptime(c_t, '%Y-%m-%d')
+                if min_time is None and max_time is None:
+                    min_time = t
+                    max_time = t
+                elif t < min_time: min_time = t
+                elif t > max_time: max_time = t
+        # generate time series by month
+        time_series = dict()
+        for year, month in month_year_iter(min_time[1], min_time[0], max_time[1], max_time[0]):
+            time_str = str(year) + '-' + str(month)
+            start_time = strptime(time_str + '-01', '%Y-%m-%d')
+            end_time = datetime.fromtimestamp(mktime(start_time)) + \
+                relativedelta.relativedelta(months=1)
+            end_time = end_time.timetuple()
+            edges = [(i1, i2) for i1, i2 in network.edges()
+                     for c, c_t in network[i1][i2]['time'].items()
+                     if strptime(c_t, '%Y-%m-%d') < end_time]
+            time_series[time_str] = network.edge_subgraph(edges).copy()
+        return time_series
+
 
 if __name__ == '__main__':
     cb_data = CrunchbaseData('data/crunchbase_2015.db')
     invest_network = cb_data.get_investment_network()
     coinvest_network = cb_data.convert_to_coinvest_network(invest_network)
     # get top co-investors
-    print(sorted(coinvest_network.edges(data='count'), \
+    print(sorted(coinvest_network.edges(data='count'),
                  key=itemgetter(2), reverse=True)[:20])
     # get top investors
-    print(sorted(invest_network.out_degree(invest_network.nodes), \
+    print(sorted(invest_network.out_degree(invest_network.nodes),
                  key=itemgetter(1), reverse=True)[:20])
+    # get time series for top coinvestors
+    top_coinvestor = sorted(coinvest_network.degree(coinvest_network.nodes),
+                            key=itemgetter(1), reverse=True)[:20]
+    top_coinvestor = [inv[0] for inv in top_coinvestor]
+    time_series = cb_data.generate_time_series(coinvest_network,
+                                               top_coinvestor)
 
