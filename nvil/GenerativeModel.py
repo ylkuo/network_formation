0815a15a9df2921cf060d3122c912cbc6ecbb9b2
Amin Rahimian <rahimian.amin@gmail.com>
2018-03-29 09:20:47 -0400

    NVIL recognition and generative model

---
+++ nvil/GenerativeModel.py
@@ -0,0 +1,136 @@
+import torch
+
+# import theano
+# import lasagne
+# import theano.tensor as T
+# import theano.tensor.nlinalg as Tla
+# import theano.tensor.slinalg as Tsla
+
+import numpy as np
+
+# from theano.tensor.shared_randomstreams import RandomStreams
+
+def NormalPDFmat(X,Mu,XChol,xDim):
+    ''' Use this version when X is a matrix [N x xDim] '''
+    return torch.exp(logNormalPDFmat(X,Mu,XChol,xDim))
+
+def logNormalPDFmat(X,Mu,XChol,xDim):
+    ''' Use this version when X is a matrix [N x xDim] '''
+    Lambda = torhc.inverse(torch.mm(XChol,torch.t(XChol)))
+    XMu = X-Mu
+    return (-0.5 * torch.mm(XMu, torch.mm(Lambda, torch.t(XMu)))
+                  + 0.5 * X.shape[0] * torch.log(Tla.det(Lambda))
+                  - 0.5 * np.log(2*np.pi) * X.shape[0]*xDim)
+
+def NormalPDF(X,Mu,XChol):
+    return torch.exp(logNormalPDF(X,Mu,XChol))
+
+def logNormalPDF(X,Mu,XChol):
+    Lambda = torch.inverse(torch.mm(XChol,torch.t(XChol)))  # torch.mm does matrix multiplication
+    XMu    = X-Mu
+    return (-0.5 * torch.mm(XMu, torch.mm(Lambda,torch.t(XMu)))  # torch.t transposes
+                  + 0.5 * torch.log(torch.det(Lambda))
+                  - 0.5 * np.log(2*np.pi) * X.shape[0])
+
+
+class GenerativeModel(object):
+    '''
+    Interface class for generative time-series models
+    '''
+    def __init__(self,GenerativeParams,xDim,yDim):
+
+        # input variable referencing top-down or external input
+
+        self.xDim = xDim
+        self.yDim = yDim
+
+
+        # internal RV for generating sample
+        # self.Xsamp = T.matrix('Xsamp')
+
+    def evaluateLogDensity(self):
+        '''
+        Return a theano function that evaluates the density of the GenerativeModel.
+        '''
+        raise Exception('Cannot call function of interface class')
+
+    def getParams(self):
+        '''
+        Return parameters of the GenerativeModel.
+        '''
+        raise Exception('Cannot call function of interface class')
+
+    def generateSamples(self):
+        '''
+        generates joint samples
+        '''
+        raise Exception('Cannot call function of interface class')
+
+    def __repr__(self):
+        return "GenerativeModel"
+
+
+class MixtureOfGaussians(GenerativeModel):
+    '''
+    xDim - # classes
+    yDim - dimensionality of observations
+    '''
+    def __init__(self, GenerativeParams, xDim, yDim):
+
+        super(MixtureOfGaussians, self).__init__(GenerativeParams,xDim,yDim)
+
+        # Mixture distribution
+        if 'pi' in GenerativeParams:
+            self.pi_un = torch.FloatTensor(np.asarray(GenerativeParams['pi']))
+            self.pi_un_array = np.asarray(GenerativeParams['pi'])
+            # theano.shared(value=np.asarray(GenerativeParams['pi'], dtype = theano.config.floatX), name='pi_un', borrow=True)     # cholesky of observation noise cov matrix
+        else:
+            self.pi_un = torch.FloatTensor(np.asarray(100*np.ones(xDim)))
+            self.pi_un_array = np.asarray(100 * np.ones(xDim))
+            # theano.shared(value=np.asarray(100*np.ones(xDim), dtype = theano.config.floatX), name='pi_un' ,borrow=True)     # cholesky of observation noise cov matrix
+
+        self.pi = torch.FloatTensor(self.pi_un_array/(self.pi_un_array).sum())
+
+        if 'RChol' in GenerativeParams:
+            self.RChol = torch.FloatTensor(np.asarray(GenerativeParams['RChol']))
+            # theano.shared(value=np.asarray(GenerativeParams['RChol'], dtype = theano.config.floatX), name='RChol' ,borrow=True)     # cholesky of observation noise cov matrix
+        else:
+            self.RChol = torch.FloatTensor(np.asarray(np.random.randn(xDim, yDim, yDim)/5))
+            # theano.shared(value=np.asarray(np.random.randn(xDim, yDim, yDim)/5, dtype = theano.config.floatX), name='RChol' ,borrow=True)     # cholesky of observation noise cov matrix
+
+        if 'x0' in GenerativeParams:
+            self.mu = torch.FloatTensor(np.asarray(GenerativeParams['mu'])) # theano.shared(value=np.asarray(GenerativeParams['mu'], dtype = theano.config.floatX), name='mu',borrow=True)     # set to zero for stationary distribution
+        else:
+            self.mu = torch.FloatTensor(np.asarray(GenerativeParams['mu'])) # theano.shared(value=np.asarray(np.random.randn(xDim, yDim), dtype = theano.config.floatX), name='mu', borrow=True)     # set to zero for stationary distribution
+
+
+    def sampleXY(self,_N):
+
+        _mu = np.asarray(self.mu.eval(), dtype=torch.FloatTensor)
+        _RChol = np.asarray(self.RChol.eval())
+        _pi = torch.clamp(self.pi, 0.001, 0.999).eval()
+        # z = torch.clamp(x, 0, 1) will return a new Tensor with the result of x bounded between 0 and 1.
+
+        b_vals = np.random.multinomial(1, _pi, size=_N)
+        x_vals = b_vals.nonzero()[1]
+
+        y_vals = np.zeros([_N, self.yDim])
+        for ii in xrange(_N):
+            y_vals[ii] = np.dot(np.random.randn(1,self.yDim), _RChol[x_vals[ii],:,:].T) + _mu[x_vals[ii]]
+
+        b_vals = np.asarray(b_vals, dtype=torch.FloatTensor)
+        y_vals = np.asarray(y_vals, dtype=torch.FloatTensor)
+
+        return [b_vals, y_vals]
+
+    def getParams(self):
+        return [self.RChol] + [self.mu] + [self.pi_un]
+
+    def evaluateLogDensity(self, h, Y):
+        X = h.nonzero()[1]
+        LogDensityVec = []
+        for count in range(length(Y))
+            LogDensityVeci, _ = logNormalPDF(Y[count], self.mu[X][count], self.RChol[X][count])
+            LogDensityVec += [LogDensityVeci]
+        # LogDensityVec,_ = torch.map(logNormalPDF, sequences = [Y,self.mu[X],self.RChol[X]])
+        return LogDensityVec + torch.log(self.pi[X])
---
+++ nvil/RecognitionModel.py
@@ -0,0 +1,125 @@
+import sys
+sys.path.append('../lib/')
+
+import torch
+
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+# import theano
+# import lasagne
+# from theano import tensor as T, function, printing
+# import theano.tensor.nlinalg as Tla
+import numpy as np
+# import theano.tensor.slinalg as Tsla
+
+class RecognitionModel(object):
+    '''
+    Recognition Model Interace Class
+
+    Recognition model approximates the posterior given some observations
+
+    Different forms of recognition models will have this interface
+
+    The constructor must take the Input Theano variable and create the
+    appropriate sampling expression.
+    '''
+
+    def __init__(self,Input,indices,xDim,yDim):
+
+        self.xDim = xDim
+        self.yDim = yDim
+        self.Input = Input
+        self.indices = indices
+
+    # def evalEntropy(self):
+    #     '''
+    #     Evaluates entropy of posterior approximation
+    #
+    #     H(q(x))
+    #
+    #     This is NOT normalized by the number of samples
+    #     '''
+    #     raise Exception("Please implement me. This is an abstract method.")
+
+    # def getParams(self):
+    #     '''
+    #     Returns a list of Theano objects that are parameters of the
+    #     recognition model. These will be updated during learning
+    #     '''
+    #     return self.params
+
+    def getSample(self):
+        '''
+        Returns a Theano object that are samples from the recognition model
+        given the input
+        '''
+        raise Exception("Please implement me. This is an abstract method.")
+
+    # def setTrainingMode(self):
+    #     '''
+    #     changes the internal state so that `getSample` will possibly return
+    #     noisy samples for better generalization
+    #     '''
+    #     raise Exception("Please implement me. This is an abstract method.")
+    #
+    # def setTestMode(self):
+    #     '''
+    #     changes the internal state so that `getSample` will supress noise
+    #     (e.g., dropout) for prediction
+    #     '''
+    #     raise Exception("Please implement me. This is an abstract method.")
+
+
+class GMMRecognition(RecognitionModel,NeuralNetworkModel):
+
+    def __init__(self,RecognitionParams,Input,xDim,yDim):
+        '''
+        h = Q_phi(x|y), where phi are parameters, x is our latent class, and y are data
+        '''
+        super(GMMRecognition, self).__init__(Input,None,xDim,yDim)
+        # self.N = Input.shape[0]                not used anywhere!
+
+        self.h = self.forward(self.Input)
+
+    # def getParams(self):
+    #     network_params = lasagne.layers.get_all_params(self.NN_h)
+    #     return network_params
+
+    def getSample(self, Y):
+        pi=T.clip(self.h, 0.001, 0.999).eval({self.Input:Y})
+        pi= (1/pi.sum(axis=1))[:, np.newaxis]*pi #enforce normalization (undesirable; for numerical stability)
+        x_vals = np.zeros([pi.shape[0],self.xDim])
+        for ii in xrange(pi.shape[0]):
+            x_vals[ii,:] = np.random.multinomial(1, pi[ii], size=1) #.nonzero()[1]
+
+        return x_vals.astype(bool)
+
+    def evalLogDensity(self, hsamp):
+
+        ''' We assume each sample is a single multinomial sample from the latent h, so each sample is an integer class.'''
+        return torch.log((self.h*hsamp).sum(axis=1))
+
+
+class NeuralNetworkModel(nn.Module):
+
+    # rec_nn = lasagne.layers.DenseLayer(rec_nn, 100, nonlinearity=leaky_rectify, W=lasagne.init.Orthogonal())
+    # rec_nn = lasagne.layers.DenseLayer(rec_nn, xDim, nonlinearity=softmax, W=lasagne.init.Orthogonal(),
+    #                                    b=-5 * np.ones(xDim, dtype=theano.config.floatX))
+    #
+    # NN_Params = dict([('network', rec_nn)])
+    # recDict = dict([('NN_Params', NN_Params)])
+    # self.NN_h = RecognitionParams['NN_Params']['network']
+
+    def __init__(self, yDim=2, nCUnits=100):
+        super(BiasCorrectNet, self).__init__()
+        self.lin1 = nn.Linear(yDim, nCUnits)
+        self.lin2 = nn.Linear(nCUnits, 1)
+        self.nonlinearity1 = nn.LeakyReLU(0.1)
+        self.nonlinearity2 = nn.Softmax()
+
+    def forward(self, x):
+        y1 = self.nonlinearity1(self.lin1(x))
+        y2 = self.nonlinearity2(self.lin2(y1))
+        return y2
