fix backprop errors

nvil_network_formation/GenerativeModel.py
@@ -228,7 +228,7 @@ class NetworkFormationGenerativeModel(UtilityModel):
             yield params
 
     def update_pi(self):
-        print('self.pi in update_pi',self.pi)
+        #print('self.pi in update_pi',self.pi)
         self.pi = self.pi_un / self.pi_un.sum()
 
     def normal_cdf(self, value):
@@ -281,7 +281,7 @@ class NetworkFormationGenerativeModel(UtilityModel):
             # on the initial condition)
         elif len(list_common_neighbors) == 0:
             epsilon_upperbound = - self.params['theta_0'] + (1 / self.params['sparsity']) * distance
-            print(epsilon_upperbound,epsilon_upperbound.shape)
+            #print(epsilon_upperbound,epsilon_upperbound.shape)
             probability_of_the_edge = self.normal_cdf(epsilon_upperbound) # needs to be fixed!!
         elif len(list_common_neighbors) > 0:
             product_term = 1
@@ -317,25 +317,24 @@ class NetworkFormationGenerativeModel(UtilityModel):
         # print('h',h)
         X = torch.t(h.nonzero())[1]
         # print('X',X)
-        log_density = []
-        for count in range(1): #range(Y.shape[0]):
-            network_time_series = Y['network'] #[count]['network']
-            last_network = network_time_series[-1]
-            unformed_edges = NX.non_edges(last_network)
-            formed_edges = NX.edges(last_network)
-            LogDensityVeci = 0
+        #log_density = []
+        #for count in range(1): #range(Y.shape[0]):
+        network_time_series = Y['network'] #[count]['network']
+        last_network = network_time_series[-1]
+        unformed_edges = NX.non_edges(last_network)
+        formed_edges = NX.edges(last_network)
+        LogDensityVeci = 0
 
-            for non_edge in unformed_edges:
-                LogDensityVeci += torch.log(self.non_edge_probability(non_edge, last_network, X)) # X[count]
+        for non_edge in unformed_edges:
+            LogDensityVeci += torch.log(self.non_edge_probability(non_edge, last_network, X)) # X[count]
 
-            for edge in formed_edges:
-                LogDensityVeci += torch.log(self.edge_probability(edge, network_time_series, last_network, X)) # X[count]
+        for edge in formed_edges:
+            LogDensityVeci += torch.log(self.edge_probability(edge, network_time_series, last_network, X)) # X[count]
 
+        # print('pi',self.pi)
+        # print('X',X)
+        # print('pi', self.pi)
+        # print('self.pi[X]', self.pi[X])
+        log_density = LogDensityVeci + torch.log(self.pi[X]) # X[count]
 
-            # print('pi',self.pi)
-            # print('X',X)
-            # print('pi', self.pi)
-            # print('self.pi[X]', self.pi[X])
-            log_density += [LogDensityVeci + torch.log(self.pi[X])] # X[count]
-
-        return torch.squeeze(torch.stack(log_density))
+        return log_density #torch.squeeze(torch.stack(log_density))

nvil_network_formation/nvil.py
@@ -32,10 +32,11 @@ class bias_correction_RNN(nn.Module):
 
     def forward(self, input, hidden):
         output_r, _ = self.rec(input, hidden)  # the recurrent units output
-        output_n = self.nonlin(output_r[-1])  # non-linearity after the recurrent units
+        output_r.view(-1, self.hidden_size)
+        output_n = self.nonlin(output_r)  # non-linearity after the recurrent units
         # self.softmax(self.out(output[-1])) #F.log_softmax(self.out(output[-1]))
         output = self.lin2(self.lin1(output_n)) # final linear layers
-        print('output of bias correction RNN',output)
+        #print('output of bias correction RNN',output)
         return output
 
     def initHidden(self):
@@ -97,14 +98,14 @@ class NVIL():
         hidden0 = self.bias_correction.initHidden()
         input_degrees = Y['degrees'].unsqueeze(0)
         input_degrees = input_degrees.permute(1, 0, 2)
-        print('input_degrees in bias correction',input_degrees)
+        #print('input_degrees in bias correction',input_degrees)
         C_out = torch.squeeze(self.bias_correction.__call__(Variable(input_degrees), hidden0))
-        print('C_out in bias correction',C_out)
+        #print('C_out in bias correction',C_out)
         L = p_yh.mean() - q_hgy.mean()
-        print('q_hgy', q_hgy)
-        print('p_yh', p_yh)
+        #print('q_hgy', q_hgy)
+        #print('p_yh', p_yh)
         l = p_yh - q_hgy - C_out
-        print('l',l)
+        #print('l',l)
         return [L, l, p_yh, q_hgy, C_out]
 
     def update_cv(self, l):
@@ -122,10 +123,7 @@ class NVIL():
             else:
                 lii = l[i].data - self.c
             lii = Variable(torch.FloatTensor(lii), requires_grad=False)
-            print('p_yh', p_yh)
             loss_p = p_yh[i] * -1
-            print('p_yh[i]', p_yh[i])
-            print('loss_p',loss_p)
             loss_p.backward(retain_graph=True)
             loss_q = q_hgy[i] * lii * -1
             loss_q.backward(retain_graph=True)
@@ -142,13 +140,13 @@ class NVIL():
             sys.stdout.flush()
             batch_counter = 0
             for data_x, data_y in data_loader:
-                print('data_x', data_x)
-                print('data_y', data_y)
+                #print('data_x', data_x)
+                #print('data_y', data_y)
                 hsamp_np = self.recognition_model.getSample(data_y)
-                print('hsamp_np',hsamp_np)
+                #print('hsamp_np',hsamp_np)
                 L, l, p_yh, q_hgy, C_out = self.get_nvil_cost(data_y, hsamp_np)
                 self.update_cv(l)
-                print('data_y fed into update_params',data_y)
+                #print('data_y fed into update_params',data_y)
                 self.update_params(1, l, p_yh, q_hgy, C_out)# data_y.shape[0] for batch_size
                 if np.mod(batch_counter, 10) == 0:
                     cx = self.c